85fc9093186d332a0e12173856e3317fabb4fee3
Ragini Gopchandani <raginigopchandani@gmail.com>
2026-03-29 06:58:39 +1100

    Exclude vite.config.ts from webpack ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ const config = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules|vite\.config\.ts/,
       },
       {
         test: /\.css$/,
